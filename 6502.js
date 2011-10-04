0d468696a2435c4780652c2bd31406585902b3de
Matt Godbolt <mgodbolt@drwholdings.com>
2011-10-04 18:14:13 -0500

    First instruction decode

--- 6502.js
+++ 6502.js
@@ -6,6 +6,26 @@ function hexword(value) {
     return hexbyte(value>>8) + hexbyte(value & 0xff);
 }
 
+function flags() {
+    this.reset = function() {
+        this.c = this.z = this.i = this.d = this.v = this.n = false;
+    }
+    this.debugString = function() {
+        return (this.c ? "C" : "_") +
+               (this.z ? "Z" : "_") +
+               (this.i ? "I" : "_") +
+               (this.d ? "D" : "_") +
+               (this.v ? "V" : "_") +
+               (this.n ? "N" : "_");
+    }
+
+    this.reset();
+}
+
+function inst_LDA_imm(cpu) {
+    console.log("Yay");
+    cpu.halted = true;
+}
 
 function cpu6502() {
     this.ramBank = new Uint8Array(16);
@@ -15,15 +35,23 @@ function cpu6502() {
     this.vis20k = 0;
     this.romOffset = 128 * 1024;
     this.osOffset = this.romOffset + 16 * 1024;
+    this.a = this.x = this.y = this.s = 0;
+    this.romsel = 0;
 
     this.readmem = function(addr) {
         if (this.debugRead) this.debugRead(addr);
         var offset = this.memlook[this.vis20k][addr >> 8];
         if (offset !== 0xffffffff) {
-            console.log(offset);
             return this.ramRomOs[offset + addr];
         }
         return null;
+    }
+
+    this.dumpregs = function() {
+        console.log("6502 registers :");
+        console.log("A=" + hexbyte(this.a) + " X=" + hexbyte(this.x) + " Y=" + hexbyte(this.y)
+                + "S=01" + hexbyte(this.s) + " PC=" + hexword(this.pc));
+        console.log("ROMSEL " + hexbyte(this.romsel>>24));
     }
 
     this.loadOs = function(name) {
@@ -57,12 +85,111 @@ function cpu6502() {
         this.cycles = 0;
         this.ram4k = this.ram8k = this.ram12k = this.ram20k = 0;
         this.pc = this.readmem(0xfffc) | (this.readmem(0xfffd)<<8);
-        console.log("Starting PC = " + hexword(this.pc));
-        this.p = {i: 1};
+        this.p = new flags();
+        this.p.i = 1;
         this.nmi = this.oldnmi = this.nmilock = 0;
         this.output = 0;
         this.tubecycle = this.tubecycles = 0;
+        this.halted = false;
+        this.instructions = [];
+        this.instructions[0xa9] = inst_LDA_imm;
+        // TODO: cpu type support.
+        console.log("Starting PC = " + hexword(this.pc));
     };
+
+    this.incpc = function() {
+        this.pc = (this.pc + 1) & 0xffff;
+    }
+
+    this.getsw = function() {
+        var result = readmem(this.pc);
+        this.incpc();
+        result |= readmem(this.pc) << 8;
+        this.incpc();
+        return result;
+    }
+
+    this.setzn = function(v) {
+        this.p.z = (v != 0);
+        this.p.n = (v & 0x80) == 0x80;
+    }
+
+    this.push = function(v) {
+        this.writemem(0x100 + this.s, v);
+        this.s = (this.s - 1) & 0xff;
+    }
+
+    this.pull = function() {
+        this.s = (this.s + 1) & 0xff;
+        return readmem(0x100 + this.s);
+    }
+
+    this.polltime = function(cycles) {
+        this.cycles -= cycles;
+        // TODO: lots more...
+    }
+
+    this.adc = function(addend, isC) {
+        if (!this.p.d) {
+            var tempw = (this.a + addend + (this.p.c ? 1 : 0)) & 0xffff;
+            this.p.v = !((this.a ^ addend) & 0x80) && !!((this.a ^ tempw) & 0x80);
+            this.a = tempw & 0xff;
+            this.p.c = !!(tempw & 0x100);
+            this.setzn(a);
+        } else {
+            var ah = 0;
+            var tempb = (this.a + addend + (p.c ? 1 : 0)) & 0xff;
+            if (!isC && !tempb) this.p.z = true;
+            var al = (this.a & 0xf) + (addend & 0xf) + (p.c ? 1 : 0);
+            if (al > 9) {
+                al -= 10;
+                al &= 0xf;
+                ah = 1;
+            }
+            ah += (this.a >> 4) + (addend >> 4);
+            if (!isC && (ah & 8)) this.p.n = true;
+            this.p.v = !((this.a ^ addend) & 0x80) && !!((this.a ^ (ah << 4)) & 0x80);
+            this.p.c = false;
+            if (ah > 9) {
+                this.p.c = true;
+                ah -= 10;
+                ah &= 0xf;
+            }
+            this.a = ((al & 0xf) | (ah << 4)) & 0xff;
+            if (isC) {
+                this.setzn(this.a);
+                this.polltime(1);
+            }
+        }
+    }
+
+    this.execute = function() {
+        if (this.halted) return;
+        this.cycles += 40000;
+        while (!this.halted && this.cycles > 0) {
+            this.pc3 = this.oldoldpc;
+            this.oldoldpc = this.oldpc;
+            this.oldpc = this.pc;
+            this.vis20k = this.ramBank[this.pc>>12];
+            var opcode = this.readmem(this.pc);
+            if (this.debugInstruction) this.debugInstruction();
+            var instruction = this.instructions[opcode];
+            if (!instruction) {
+                console.log("Invalid opcode " + hexbyte(opcode) + " at " + hexword(this.pc));
+                this.dumpregs();
+                this.halted = true;
+                return;
+            }
+            this.incpc();
+            instruction(this);
+            // TODO: timetolive
+            // TODO: interrupts
+            this.interrupt &= 0x7f;
+            // TODO: otherstuff
+            // TODO: tube
+            // TODO: nmis
+        }
+    }
 
     this.loadOs("roms/os");
     this.reset();
--- main.js
+++ main.js
@@ -10,4 +10,5 @@ $(function() {
     imageData = ctx.getImageData(0, 0, 1280, 768);
     imageDataData = imageData.data;
     processor = new cpu6502();
+    processor.execute();
 })
