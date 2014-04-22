ab776529597cf51b2366fbc73da608d397497572
Matt Godbolt <mgodbolt@drwholdings.com>
2014-04-21 22:28:46 -0500

    Add and enable one of the three Kevin Edwards protection systems. Thanks to Kevin for letting me add it here.

--- tests/test.js
+++ tests/test.js
@@ -254,7 +254,7 @@ $(function() {
 
     var tests = [
     { test: "Test timings", func: testTimings },
-//    { test: "Alien8 protection", func: function(whenDone) { testKevinEdwards("ALIEN8", whenDone); } },
+    { test: "Alien8 protection", func: function(whenDone) { testKevinEdwards("ALIEN8", whenDone); } },
 //    { test: "Nightshade protection", func: function(whenDone) { testKevinEdwards("NIGHTSH", whenDone); } },
 //    { test: "Lunar Jetman protection", func: function(whenDone) { testKevinEdwards("JETMAN", whenDone); } },
         ];
