1c4a826c0544f3c8765d28e467667d3476a05a16
Olly Betts <olly@survex.com>
2021-01-01 12:29:40 +1300

    Allow tokenising longer input lines
    We used the tokenisation code in the BASIC ROM, which calls a subroutine
    to copy down the tail of the line after substituting a token, using a loop
    indexed on Y.  This limits the untokenised tail to 255 bytes (plus a
    terminating CR) and makes tokenising O(n²) for a line with a lot of tokens.
    
    Intercept the instruction which calls this subroutine, copy out the
    newly processed bytes, adjust the zero page pointer and resume executing
    at the instruction after the subroutine call.  This allows tokenising
    arbitrarily long input provided the result fits within the BASIC line
    length limit, and is faster.

--- basic-tokenise.js
+++ basic-tokenise.js
@@ -19,13 +19,34 @@ define(['utils', 'models', 'fake6502', 'promise'],
                 }
                 cpu.writemem(offset + line.length, 0x0d);
                 var safety = 20 * 1000 * 1000;
+                var result = "";
                 while (cpu.s <= 0xf0) {
                     cpu.execute(1);
                     if (--safety === 0) {
                         break;
                     }
+                    if (cpu.pc === 0x8ea1) {
+                        // Intercept the subroutine in the BASIC ROM which replaces a keyword
+                        // with a token and copies down the tail of the line.  The 6502 code
+                        // uses the Y register to index the copy and fails if the untokenised
+                        // tail is longer than 255 bytes.  It also makes tokenisation O(n²)
+                        // for a line with a lot of tokens.
+                        //
+                        // Instead we copy out the newly processed part and advance the pointer
+                        // in 0x37/0x38 to the unprocessed part.
+                        let to = cpu.readmemZpStack(0x38) << 8 | cpu.readmemZpStack(0x37);
+                        while (offset < to) {
+                            result += String.fromCharCode(cpu.readmem(offset));
+                            offset++;
+                        }
+                        result += String.fromCharCode(cpu.a);
+                        offset += cpu.y;
+                        cpu.writememZpStack(0x37, offset & 0xff);
+                        cpu.writememZpStack(0x38, (offset >>> 8) & 0xff);
+                        ++offset;
+                        cpu.pc = 0x8ea4;
+                    }
                 }
-                var result = "";
                 for (i = offset; cpu.readmem(i) !== 0x0d; ++i) {
                     result += String.fromCharCode(cpu.readmem(i));
                 }
@@ -39,6 +60,9 @@ define(['utils', 'models', 'fake6502', 'promise'],
                 var lineSplit = line.match(lineRe);
                 var lineNum = lineSplit[1] ? parseInt(lineSplit[1]) : lineNumIfNotSpec;
                 var tokens = callTokeniser(lineSplit[2]);
+                if (tokens.length > 251) {
+                    throw new Error("Line " + lineNum + " tokenised length " + tokens.length + " > 251 bytes");
+                }
                 return '\r' +
                     String.fromCharCode((lineNum >>> 8) & 0xff) +
                     String.fromCharCode(lineNum & 0xff) +
