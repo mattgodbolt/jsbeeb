e9431a38d085f3979485e86e5ec88ea22baed859
Matt Godbolt <matt@godbolt.org>
2021-09-09 08:20:22 -0500

    Formatting; tiny tweak to fetch

--- three-canvas.js
+++ three-canvas.js
@@ -246,7 +246,7 @@ define(['three', 'jquery', 'utils', 'three-mtl-loader', 'three-obj-loader', 'thr
 
             const newUniforms = {
                 maskTexture: {type: "t", value: this.maskTexture}
-            };            
+            };
 
             const screenPrologFragment = this.screenPrologFragment;
             const screenEmissiveFragment = this.screenEmissiveFragment;
@@ -259,7 +259,7 @@ define(['three', 'jquery', 'utils', 'three-mtl-loader', 'three-obj-loader', 'thr
 
                 shader.fragmentShader = shader.fragmentShader.replace(
                     `#include <common>`,
-                    screenPrologFragment + '\n#include <common>' );
+                    screenPrologFragment + '\n#include <common>');
 
                 shader.fragmentShader = shader.fragmentShader.replace(
                     `#include <emissivemap_fragment>`,
@@ -296,7 +296,8 @@ define(['three', 'jquery', 'utils', 'three-mtl-loader', 'three-obj-loader', 'thr
         }
 
         async loadShaderSource(fileName) {
-            return fetch(fileName).then(response => response.text()).then((shaderSource) => { return shaderSource; });
+            const response = await fetch(fileName);
+            return response.text();
         }
 
         async loadModel(materials) {
