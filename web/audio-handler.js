0d9e0455909dc999c72113be000dfb5d9966d8d0
Matt Godbolt <matt@godbolt.org>
2021-12-23 09:26:56 -0600

    zomg progress!

--- web/audio-handler.js
+++ web/audio-handler.js
@@ -5,28 +5,39 @@ import {AudioWorklet} from "audio-worklet";
 export class AudioHandler {
     constructor(warningNode, audioFilterFreq, audioFilterQ, noSeek) {
         this.warningNode = warningNode;
+        this.warningNode.toggle(false);
 
         /*global webkitAudioContext*/
         this.audioContext = typeof AudioContext !== 'undefined' ? new AudioContext()
             : typeof webkitAudioContext !== 'undefined' ? new webkitAudioContext()
                 : null;
         this._jsAudioNode = null;
-        if (this.audioContext) {
+        if (this.audioContext && this.audioContext.audioWorklet) {
             this.audioContext.onstatechange = () => this.checkStatus();
             this.soundChip = new SoundChip((buffer, time) => this._onBuffer(buffer, time));
             this.ddNoise = noSeek ? new FakeDdNoise() : new DdNoise(this.audioContext);
             this._setup(audioFilterFreq, audioFilterQ).then();
         } else {
+            if (this.audioContext && !this.audioContext.audioWorklet) {
+                this.audioContext = null;
+                console.log("Unable to initialise audio: no audio worklet API");
+                this.warningNode.toggle(true);
+                const localhost = new URL(window.location);
+                localhost.hostname = 'localhost';
+                this.warningNode.html(
+                    `No audio worklet API was found - there will be no audio. 
+                    If you are running a local jsbeeb, you must either use a host of
+                    <a href="${localhost}">localhost</a>, 
+                    or serve the content over <em>https</em>.`);
+            }
             this.soundChip = new FakeSoundChip();
             this.ddNoise = new FakeDdNoise();
         }
 
         this.warningNode.on('mousedown', () => this.tryResume());
-        this.warningNode.toggle(false);
     }
 
     async _setup(audioFilterFreq, audioFilterQ) {
-        new Worker(new URL('./audio-renderer.js', import.meta.url));
         await this.audioContext.audioWorklet.addModule(
             new AudioWorklet(
                 new URL("./audio-renderer.js", import.meta.url)
