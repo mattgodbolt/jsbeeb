a745fade819567c3e975bdc0c79e28cc2795b54a
Matt Godbolt <matt@godbolt.org>
2022-09-01 20:57:46 -0500

    Port cmos to class

--- cmos.js
+++ cmos.js
@@ -1,43 +1,46 @@
 "use strict";
 
-export function Cmos(persistence) {
-    let store = null;
-    if (persistence) {
-        store = persistence.load();
-    }
-    if (!store) {
-        store = [
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0xc9, 0xff, 0xff, 0x12, 0x00, 0x17, 0xca, 0x1e, 0x05, 0x00, 0x35, 0xa6, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        ];
-        save();
-    }
-    let enabled = false;
-    let isRead = false;
-    let addressSelect = false;
-    let dataSelect = false;
-    let cmosAddr = 0;
+const defaultCmos = [
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0xc9, 0xff, 0xff, 0x12, 0x00, 0x17, 0xca, 0x1e, 0x05, 0x00, 0x35, 0xa6, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+];
 
-    function save() {
-        if (persistence) {
-            persistence.save(store);
+export class Cmos {
+    constructor(persistence) {
+        this.store = persistence ? persistence.load() : null;
+        this.persistence = persistence;
+        this.enabled = false;
+        this.isRead = false;
+        this.addressSelect = false;
+        this.dataSelect = false;
+        this.cmosAddr = 0;
+
+        if (!this.store) {
+            this.store = defaultCmos;
+            this.save();
         }
     }
 
-    function cmosRead() {
-        if (!enabled) return 0xff;
+    save() {
+        if (this.persistence) {
+            this.persistence.save(this.store);
+        }
+    }
+
+    read() {
+        if (!this.enabled) return 0xff;
         // To drive the bus we need:
         // - CMOS enabled.
         // - Address Select low.
         // - Data Select high.
         // - Read high.
 
-        if (!addressSelect && dataSelect && isRead) {
+        if (!this.addressSelect && this.dataSelect && this.isRead) {
             // The first 10 bytes of CMOS RAM store the RTC clock
-            if (cmosAddr < 10) {
+            if (this.cmosAddr < 10) {
                 const current = new Date();
-                switch (cmosAddr) {
+                switch (this.cmosAddr) {
                     // Note values are returned in BCD format
                     case 0:
                         return parseInt(current.getSeconds().toString(10), 16);
@@ -55,27 +58,24 @@ export function Cmos(persistence) {
                         return parseInt(current.getFullYear().toString(10), 16);
                 }
             } else {
-                return store[cmosAddr] & 0xff;
+                return this.store[this.cmosAddr] & 0xff;
             }
         }
         return 0xff;
     }
 
-    function cmosWriteControl(portbpins, portapins, IC32) {
-        enabled = !!(portbpins & 0x40);
-        if (!enabled) return;
-        const oldDataSelect = dataSelect;
-        const oldAddressSelect = addressSelect;
-        isRead = !!(IC32 & 2);
-        dataSelect = !!(IC32 & 4);
-        addressSelect = !!(portbpins & 0x80);
-        if (oldAddressSelect && !addressSelect) cmosAddr = portapins & 0x3f;
-        if (oldDataSelect && !dataSelect && !addressSelect && !isRead && cmosAddr > 0xb) {
-            store[cmosAddr] = portapins;
-            save();
+    writeControl(portBpins, portApins, IC32) {
+        this.enabled = !!(portBpins & 0x40);
+        if (!this.enabled) return;
+        const oldDataSelect = this.dataSelect;
+        const oldAddressSelect = this.addressSelect;
+        this.isRead = !!(IC32 & 2);
+        this.dataSelect = !!(IC32 & 4);
+        this.addressSelect = !!(portBpins & 0x80);
+        if (oldAddressSelect && !this.addressSelect) this.cmosAddr = portApins & 0x3f;
+        if (oldDataSelect && !this.dataSelect && !this.addressSelect && !this.isRead && this.cmosAddr > 0xb) {
+            this.store[this.cmosAddr] = portApins;
+            this.save();
         }
     }
-
-    this.read = cmosRead;
-    this.writeControl = cmosWriteControl;
 }
