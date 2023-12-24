e2dc521e3074b270eb8050efbefc30265f530e62
Matt Godbolt <matt@godbolt.org>
2023-12-24 09:32:36 -0600

    Class-ify teletext

--- teletext.js
+++ teletext.js
@@ -2,35 +2,38 @@
 import { makeChars } from "./teletext_data.js";
 import { makeFast32 } from "./utils.js";
 
-export function Teletext() {
-    this.prevCol = 0;
-    this.holdOff = false;
-    this.col = 7;
-    this.bg = 0;
-    this.sep = false;
-    this.dbl = this.oldDbl = this.secondHalfOfDouble = this.wasDbl = false;
-    this.gfx = false;
-    this.flash = this.flashOn = false;
-    this.flashTime = 0;
-    this.heldChar = false;
-    this.holdChar = 0;
-    this.dataQueue = [0, 0, 0, 0];
-    this.scanlineCounter = 0;
-    this.levelDEW = false;
-    this.levelDISPTMG = false;
-    this.levelRA0 = false;
+export class Teletext {
+    constructor() {
+        this.prevCol = 0;
+        this.holdOff = false;
+        this.col = 7;
+        this.bg = 0;
+        this.sep = false;
+        this.dbl = this.oldDbl = this.secondHalfOfDouble = this.wasDbl = false;
+        this.gfx = false;
+        this.flash = this.flashOn = false;
+        this.flashTime = 0;
+        this.heldChar = false;
+        this.holdChar = 0;
+        this.dataQueue = [0, 0, 0, 0];
+        this.scanlineCounter = 0;
+        this.levelDEW = false;
+        this.levelDISPTMG = false;
+        this.levelRA0 = false;
 
-    this.normalGlyphs = makeFast32(new Uint32Array(96 * 20));
-    this.graphicsGlyphs = makeFast32(new Uint32Array(96 * 20));
-    this.separatedGlyphs = makeFast32(new Uint32Array(96 * 20));
-    this.colour = makeFast32(new Uint32Array(256));
+        this.normalGlyphs = makeFast32(new Uint32Array(96 * 20));
+        this.graphicsGlyphs = makeFast32(new Uint32Array(96 * 20));
+        this.separatedGlyphs = makeFast32(new Uint32Array(96 * 20));
+        this.colour = makeFast32(new Uint32Array(256));
 
-    this.nextGlyphs = this.normalGlyphs;
-    this.curGlyphs = this.normalGlyphs;
-    this.heldGlyphs = this.normalGlyphs;
+        this.nextGlyphs = this.normalGlyphs;
+        this.curGlyphs = this.normalGlyphs;
+        this.heldGlyphs = this.normalGlyphs;
 
-    this.init = function () {
-        let c;
+        this.init();
+    }
+
+    init() {
         const charData = makeChars();
 
         // Build palette
@@ -105,7 +108,7 @@ export function Teletext() {
         }
 
         // Build graphics character set
-        for (c = 0; c < 96; ++c) {
+        for (let c = 0; c < 96; ++c) {
             if (!(c & 32)) {
                 setGraphicsBlock(c, 0, 0, 3, 3, false, !!(c & 1));
                 setGraphicsBlock(c, 3, 0, 3, 3, false, !!(c & 2));
@@ -119,7 +122,7 @@ export function Teletext() {
         makeHiResGlyphs(this.graphicsGlyphs, true);
 
         // Build separated graphics character set
-        for (c = 0; c < 96; ++c) {
+        for (let c = 0; c < 96; ++c) {
             if (!(c & 32)) {
                 setGraphicsBlock(c, 0, 0, 3, 3, true, !!(c & 1));
                 setGraphicsBlock(c, 3, 0, 3, 3, true, !!(c & 2));
@@ -131,217 +134,215 @@ export function Teletext() {
         }
 
         makeHiResGlyphs(this.separatedGlyphs, true);
-    };
-    this.init();
-}
+    }
 
-Teletext.prototype.setNextChars = function () {
-    if (this.gfx) {
-        if (this.sep) {
-            this.nextGlyphs = this.separatedGlyphs;
+    setNextChars() {
+        if (this.gfx) {
+            if (this.sep) {
+                this.nextGlyphs = this.separatedGlyphs;
+            } else {
+                this.nextGlyphs = this.graphicsGlyphs;
+            }
         } else {
-            this.nextGlyphs = this.graphicsGlyphs;
+            this.nextGlyphs = this.normalGlyphs;
         }
-    } else {
-        this.nextGlyphs = this.normalGlyphs;
-    }
-};
-
-Teletext.prototype.handleControlCode = function (data) {
-    this.holdOff = false;
-
-    switch (data) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-            this.gfx = false;
-            this.col = data;
-            this.setNextChars();
-            break;
-        case 8:
-            this.flash = true;
-            break;
-        case 9:
-            this.flash = false;
-            break;
-        case 12:
-        case 13:
-            this.dbl = !!(data & 1);
-            if (this.dbl) this.wasDbl = true;
-            break;
-        case 17:
-        case 18:
-        case 19:
-        case 20:
-        case 21:
-        case 22:
-        case 23:
-            this.gfx = true;
-            this.col = data & 7;
-            this.setNextChars();
-            break;
-        case 24:
-            this.col = this.prevCol = this.bg;
-            break;
-        case 25:
-            this.sep = false;
-            this.setNextChars();
-            break;
-        case 26:
-            this.sep = true;
-            this.setNextChars();
-            break;
-        case 28:
-            this.bg = 0;
-            break;
-        case 29:
-            this.bg = this.col;
-            break;
-        case 30:
-            this.holdChar = true;
-            break;
-        case 31:
-            this.holdOff = true;
-            break;
-    }
-    if (this.holdChar && this.dbl === this.oldDbl) {
-        data = this.heldChar;
-        if (data >= 0x40 && data < 0x60) data = 0x20;
-        this.curGlyphs = this.heldGlyphs;
-    } else {
-        this.heldChar = 0x20;
-        data = 0x20;
-    }
-    return data;
-};
-
-Teletext.prototype.fetchData = function (data) {
-    this.dataQueue.shift();
-    this.dataQueue.push(data & 0x7f);
-};
-
-Teletext.prototype.setDEW = function (level) {
-    // The SAA5050 input pin "DEW" is connected to the 6845 output pin
-    // "VSYNC" and it is used to track frames.
-    const oldlevel = this.levelDEW;
-    this.levelDEW = level;
-
-    // Trigger on high -> low. This appears to be what the hardware does.
-    // It needs to be this way for the scanline counter to stay in sync
-    // if you set R6>R4.
-    if (!oldlevel || level) {
-        return;
     }
 
-    this.scanlineCounter = 0;
-    this.secondHalfOfDouble = false;
+    handleControlCode(data) {
+        this.holdOff = false;
 
-    // 3:1 flash ratio.
-    if (++this.flashTime === 64) this.flashTime = 0;
-    // Flashing text starts off in sync with a slow cursor, extinguished
-    // together.  Multiple MODE changes gradually desynchronise the
-    // frame counters.
-    // TODO: this point is being reached a MOS-dependent number of times
-    // before Video.frameCount rises.  The next line achieves initial
-    // sync under MOS 1.20 only.
-    this.flashOn = this.flashTime < 16;
-};
-
-Teletext.prototype.setDISPTMG = function (level) {
-    // The SAA5050 input pin "LOSE" is connected to the 6845 output pin
-    // "DISPTMG" and it is used to track scanlines.
-    const oldlevel = this.levelDISPTMG;
-    this.levelDISPTMG = level;
-
-    // Trigger on high -> low. This is probably what the hardware does as
-    // we need to increment scanline at the end of the scanline, not the
-    // beginning.
-    if (!oldlevel || level) {
-        return;
+        switch (data) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                this.gfx = false;
+                this.col = data;
+                this.setNextChars();
+                break;
+            case 8:
+                this.flash = true;
+                break;
+            case 9:
+                this.flash = false;
+                break;
+            case 12:
+            case 13:
+                this.dbl = !!(data & 1);
+                if (this.dbl) this.wasDbl = true;
+                break;
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+                this.gfx = true;
+                this.col = data & 7;
+                this.setNextChars();
+                break;
+            case 24:
+                this.col = this.prevCol = this.bg;
+                break;
+            case 25:
+                this.sep = false;
+                this.setNextChars();
+                break;
+            case 26:
+                this.sep = true;
+                this.setNextChars();
+                break;
+            case 28:
+                this.bg = 0;
+                break;
+            case 29:
+                this.bg = this.col;
+                break;
+            case 30:
+                this.holdChar = true;
+                break;
+            case 31:
+                this.holdOff = true;
+                break;
+        }
+        if (this.holdChar && this.dbl === this.oldDbl) {
+            data = this.heldChar;
+            if (data >= 0x40 && data < 0x60) data = 0x20;
+            this.curGlyphs = this.heldGlyphs;
+        } else {
+            this.heldChar = 0x20;
+            data = 0x20;
+        }
+        return data;
     }
 
-    this.col = 7;
-    this.bg = 0;
-    this.holdChar = false;
-    this.heldChar = 0x20;
-    this.nextGlyphs = this.heldGlyphs = this.normalGlyphs;
-    this.flash = false;
-    this.sep = false;
-    this.gfx = false;
-    this.dbl = false;
+    fetchData(data) {
+        this.dataQueue.shift();
+        this.dataQueue.push(data & 0x7f);
+    }
 
-    this.scanlineCounter++;
-    // Check for end of character row.
-    if (this.scanlineCounter === 10) {
+    setDEW(level) {
+        // The SAA5050 input pin "DEW" is connected to the 6845 output pin
+        // "VSYNC" and it is used to track frames.
+        const oldLevel = this.levelDEW;
+        this.levelDEW = level;
+
+        // Trigger on high -> low. This appears to be what the hardware does.
+        // It needs to be this way for the scanline counter to stay in sync
+        // if you set R6>R4.
+        if (!oldLevel || level) {
+            return;
+        }
+
         this.scanlineCounter = 0;
+        this.secondHalfOfDouble = false;
 
-        if (this.secondHalfOfDouble) {
-            this.secondHalfOfDouble = false;
-        } else {
-            this.secondHalfOfDouble = this.wasDbl;
+        // 3:1 flash ratio.
+        if (++this.flashTime === 64) this.flashTime = 0;
+        // Flashing text starts off in sync with a slow cursor, extinguished
+        // together.  Multiple MODE changes gradually desynchronise the
+        // frame counters.
+        // TODO: this point is being reached a MOS-dependent number of times
+        // before Video.frameCount rises.  The next line achieves initial
+        // sync under MOS 1.20 only.
+        this.flashOn = this.flashTime < 16;
+    }
+
+    setDISPTMG(level) {
+        // The SAA5050 input pin "LOSE" is connected to the 6845 output pin
+        // "DISPTMG" and it is used to track scanlines.
+        const oldLevel = this.levelDISPTMG;
+        this.levelDISPTMG = level;
+
+        // Trigger on high -> low. This is probably what the hardware does as
+        // we need to increment scanline at the end of the scanline, not the
+        // beginning.
+        if (!oldLevel || level) {
+            return;
         }
-    }
 
-    this.wasDbl = false;
-};
-
-Teletext.prototype.setRA0 = function (level) {
-    // The SAA5050 input pin "CRS" is connected to the 6845 output pin
-    // "RA0", via a signal inverter, and it is used to select between a
-    // normal scanline and a calculated smoothing scanline.
-    this.levelRA0 = level;
-};
-
-Teletext.prototype.render = function (buf, offset) {
-    let data = this.dataQueue[0];
-
-    let scanline = this.scanlineCounter << 1;
-    if (this.levelRA0) {
-        scanline++;
-    }
-
-    this.oldDbl = this.dbl;
-
-    this.prevCol = this.col;
-    this.curGlyphs = this.nextGlyphs;
-
-    const prevFlash = this.flash;
-    if (data < 0x20) {
-        data = this.handleControlCode(data);
-    } else if (this.gfx) {
-        this.heldChar = data;
-        this.heldGlyphs = this.curGlyphs;
-    }
-
-    if (this.oldDbl) {
-        scanline = scanline >>> 1;
-        if (this.secondHalfOfDouble) {
-            scanline += 10;
-        }
-    }
-    let chardef = this.curGlyphs[(data - 32) * 20 + scanline];
-
-    if ((prevFlash && this.flashOn) || (this.secondHalfOfDouble && !this.dbl)) {
-        const backgroundColour = this.colour[(this.bg & 7) << 5];
-        for (let i = 0; i < 16; ++i) {
-            buf[offset++] = backgroundColour;
-        }
-    } else {
-        const paletteIndex = ((this.bg & 7) << 5) | ((this.prevCol & 7) << 2);
-
-        // TODO: see if we should unroll here (we used to, before it got more complex).
-        for (let pixel = 0; pixel < 16; ++pixel) {
-            buf[offset + pixel] = this.colour[paletteIndex + (chardef & 3)];
-            chardef >>>= 2;
-        }
-    }
-
-    if (this.holdOff) {
+        this.col = 7;
+        this.bg = 0;
         this.holdChar = false;
-        this.heldChar = 32;
+        this.heldChar = 0x20;
+        this.nextGlyphs = this.heldGlyphs = this.normalGlyphs;
+        this.flash = false;
+        this.sep = false;
+        this.gfx = false;
+        this.dbl = false;
+
+        this.scanlineCounter++;
+        // Check for end of character row.
+        if (this.scanlineCounter === 10) {
+            this.scanlineCounter = 0;
+
+            if (this.secondHalfOfDouble) {
+                this.secondHalfOfDouble = false;
+            } else {
+                this.secondHalfOfDouble = this.wasDbl;
+            }
+        }
+
+        this.wasDbl = false;
     }
-};
+
+    setRA0(level) {
+        // The SAA5050 input pin "CRS" is connected to the 6845 output pin
+        // "RA0", via a signal inverter, and it is used to select between a
+        // normal scanline and a calculated smoothing scanline.
+        this.levelRA0 = level;
+    }
+
+    render(buf, offset) {
+        let data = this.dataQueue[0];
+
+        let scanline = this.scanlineCounter << 1;
+        if (this.levelRA0) {
+            scanline++;
+        }
+
+        this.oldDbl = this.dbl;
+
+        this.prevCol = this.col;
+        this.curGlyphs = this.nextGlyphs;
+
+        const prevFlash = this.flash;
+        if (data < 0x20) {
+            data = this.handleControlCode(data);
+        } else if (this.gfx) {
+            this.heldChar = data;
+            this.heldGlyphs = this.curGlyphs;
+        }
+
+        if (this.oldDbl) {
+            scanline = scanline >>> 1;
+            if (this.secondHalfOfDouble) {
+                scanline += 10;
+            }
+        }
+        let chardef = this.curGlyphs[(data - 32) * 20 + scanline];
+
+        if ((prevFlash && this.flashOn) || (this.secondHalfOfDouble && !this.dbl)) {
+            const backgroundColour = this.colour[(this.bg & 7) << 5];
+            for (let i = 0; i < 16; ++i) {
+                buf[offset++] = backgroundColour;
+            }
+        } else {
+            const paletteIndex = ((this.bg & 7) << 5) | ((this.prevCol & 7) << 2);
+
+            for (let pixel = 0; pixel < 16; ++pixel) {
+                buf[offset + pixel] = this.colour[paletteIndex + (chardef & 3)];
+                chardef >>>= 2;
+            }
+        }
+
+        if (this.holdOff) {
+            this.holdChar = false;
+            this.heldChar = 32;
+        }
+    }
+}
