fa699a847c9857b7743587de74fdf12a6bd097e5
Matt Godbolt <matt@godbolt.org>
2024-09-13 11:46:57 -0500

    Add more tests

--- tests/integration/via.js
+++ tests/integration/via.js
@@ -14,13 +14,22 @@ async function runViaProgram(source) {
     return testMachine;
 }
 
+const resultAddress = 0x100;
+
+function expectArray(testMachine, array) {
+    let addr = resultAddress;
+    for (const expected of array) {
+        assert.equal(testMachine.readbyte(addr), expected);
+        addr++;
+    }
+}
+
 describe("should pass scarybeasts' VIA tests", function () {
     // Code here extracted and paraphrased from the SSD zipfile from https://github.com/mattgodbolt/jsbeeb/issues/179
     it("VIA.AC1 - Does ACR write restart timer?", async function () {
-        // Real BBC: 64, 0, 0, 128
         const testMachine = await runViaProgram(`
 DIM MC% 100
-R% = &200
+R% = ${resultAddress}
 P% = MC%
 [
 OPT 2
@@ -62,16 +71,12 @@ PRINT ?(R%)
 PRINT ?(R%+1)
 PRINT ?(R%+2)
 PRINT ?(R%+3)`);
-        assert.equal(testMachine.readbyte(0x200), 64);
-        assert.equal(testMachine.readbyte(0x201), 0);
-        assert.equal(testMachine.readbyte(0x202), 0);
-        assert.equal(testMachine.readbyte(0x203), 128);
+        expectArray(testMachine, [64, 0, 0, 128]);
     });
     it("VIA.AC2 - Does ACR write at timer expiry affect behaviour #1?", async function () {
-        // Real BBC: 0, 0
         const testMachine = await runViaProgram(`
 DIM MC% 100
-R% = &200
+R% = ${resultAddress}
 P% = MC%
 [
 OPT 2
@@ -98,7 +103,448 @@ PRINT "VIA TEST: DOES ACR WRITE AT TIMER EXPIRY AFFECT BEHAVIOR #1?"
 PRINT "REAL BBC: 0, 0"
 PRINT ?(R%)
 PRINT ?(R%+1)`);
-        assert.equal(testMachine.readbyte(0x200), 0);
-        assert.equal(testMachine.readbyte(0x201), 0);
+        expectArray(testMachine, [0, 0]);
+    });
+    it("VIA.AC3 - Does ACR write at timer expiry affect behaviour #2?", async function () {
+        const testMachine = await runViaProgram(`
+DIM MC% 100
+R% = ${resultAddress}
+P% = MC%
+[
+OPT 2
+SEI
+LDA #&7F
+STA &FE6E
+LDA #&40
+STA &FE6B
+LDA #&02
+STA &FE64
+LDA #&00
+STA &FE65
+LDA #&00
+STA &FE6B
+LDA &FE64
+STA R%
+LDA &FE6D
+STA R%+1
+CLI
+RTS
+]
+CALL MC%
+PRINT "VIA TEST: DOES ACR WRITE AT TIMER EXPIRY AFFECT BEHAVIOR #2?"
+PRINT "REAL BBC: 0, 0"
+PRINT ?(R%)
+PRINT ?(R%+1)`);
+        expectArray(testMachine, [0, 0]);
+    });
+    it("VIA.AC4 - flip ACR mode to one-shot after expiry, does it IRQ?", async function () {
+        const testMachine = await runViaProgram(`
+DIM MC% 256
+R% = ${resultAddress}
+P% = MC%
+[
+OPT 2
+SEI
+LDA #&7F
+STA &FE6E
+LDA #&40
+STA &FE6B
+LDA #10
+STA &FE64
+LDA #&00
+STA &FE65
+LDA &FE6D
+STA R%
+LDA &FE64
+STA R%+1
+NOP
+NOP
+LDA &FE6D
+STA R%+2
+LDA &FE64
+STA R%+3
+NOP
+NOP
+LDA &FE6D
+STA R%+4
+LDA &FE64
+STA R%+5
+NOP
+NOP
+LDA #&00
+STA &FE6B
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+LDA &FE6D
+STA R%+6
+LDA &FE64
+STA R%+7
+NOP
+NOP
+LDA &FE6D
+STA R%+8
+LDA &FE64
+STA R%+9
+CLI
+RTS
+]
+CALL MC%
+REM REAL BBC! 0, 3, 64, 3, 64, 3, 64, 3, 0, 3
+PRINT "VIA TEST: FLIP ACR MODE TO ONE-SHOT AFTER EXPIRY, DOES IT IRQ?"
+PRINT "REAL BBC: 0, 3, 64, 3, 64, 3, 64, 3, 0, 3"
+PRINT ?(R%)
+PRINT ?(R%+1)
+PRINT ?(R%+2)
+PRINT ?(R%+3)
+PRINT ?(R%+4)
+PRINT ?(R%+5)
+PRINT ?(R%+6)
+PRINT ?(R%+7)
+PRINT ?(R%+8)
+PRINT ?(R%+9)`);
+        expectArray(testMachine, [0, 3, 64, 3, 64, 3, 64, 3, 0, 3]);
+    });
+    it("VIA.AC5 - flip ACR mode to continuous after expiry, does it IRQ?", async function () {
+        const testMachine = await runViaProgram(`
+DIM MC% 256
+R% = ${resultAddress}
+P% = MC%
+[
+OPT 2
+SEI
+LDA #&7F
+STA &FE6E
+LDA #&00
+STA &FE6B
+LDA #10
+STA &FE64
+LDA #&00
+STA &FE65
+LDA &FE6D
+STA R%
+LDA &FE64
+STA R%+1
+NOP
+NOP
+LDA &FE6D
+STA R%+2
+LDA &FE64
+STA R%+3
+NOP
+NOP
+LDA &FE6D
+STA R%+4
+LDA &FE64
+STA R%+5
+NOP
+NOP
+LDA #&40
+STA &FE6B
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+LDA &FE6D
+STA R%+6
+LDA &FE64
+STA R%+7
+NOP
+NOP
+LDA &FE6D
+STA R%+8
+LDA &FE64
+STA R%+9
+CLI
+RTS
+]
+CALL MC%
+REM REAL BBC! 0, 3, 64, 3, 0, 3, 0, 3, 0, 3
+PRINT "VIA TEST: FLIP ACR MODE TO CONT AFTER EXPIRY, DOES IT IRQ?"
+PRINT "REAL BBC: 0, 3, 64, 3, 0, 3, 0, 3, 0, 3"
+PRINT ?(R%)
+PRINT ?(R%+1)
+PRINT ?(R%+2)
+PRINT ?(R%+3)
+PRINT ?(R%+4)
+PRINT ?(R%+5)
+PRINT ?(R%+6)
+PRINT ?(R%+7)
+PRINT ?(R%+8)
+PRINT ?(R%+9)`);
+        expectArray(testMachine, [0, 3, 64, 3, 0, 3, 0, 3, 0, 3]);
+    });
+    it("VIA.AC6 - getting to the bottom of ACR write at IRQ time #1", async function () {
+        const testMachine = await runViaProgram(`
+DIM MC% 256
+R% = ${resultAddress}
+P% = MC%
+[
+OPT 0
+SEI
+LDA #&7F
+STA &FE6E
+LDA #6
+STA &FE64
+
+LDA #&40
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&00
+STA &FE6B
+NOP : NOP : NOP : NOP
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%
+STX R%+1
+STY R%+2
+
+LDA #&40
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&00
+NOP : NOP : NOP : NOP
+STA &FE6B
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+3
+STX R%+4
+STY R%+5
+
+LDA #&40
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&00
+NOP : NOP : NOP
+STA &FE6B
+NOP
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+6
+STX R%+7
+STY R%+8
+
+LDA #&40
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&00
+LDX &00
+NOP : NOP : NOP
+STA &FE6B
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+9
+STX R%+10
+STY R%+11
+
+LDA #&40
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&C0
+NOP : NOP : NOP : NOP
+STA &FE6B
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+12
+STX R%+13
+STY R%+14
+CLI
+RTS
+]
+CALL MC%
+PRINT "VIA TEST: GETTING TO THE BOTTOM OF ACR WRITE AT IRQ TIME #1"
+PRINT "REAL BBC: 64, 0, 1, 64, 0, 1, 64, 0, 1, 64, 0, 1, 64, 64, 1"
+PRINT "40 -> 00 IMMEDIATE"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%)
+PRINT ?(R%+1)
+PRINT ?(R%+2)
+PRINT "40 -> 00 AT -1"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%+3)
+PRINT ?(R%+4)
+PRINT ?(R%+5)
+PRINT "40 -> 00 AT 0"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%+6)
+PRINT ?(R%+7)
+PRINT ?(R%+8)
+PRINT "40 -> 00 AT -1, 2 CYCLE STORE"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%+9)
+PRINT ?(R%+10)
+PRINT ?(R%+11)
+PRINT "40 -> C0 AT -1"
+REM REAL BBC! 64, 64, 1
+PRINT ?(R%+12)
+PRINT ?(R%+13)
+PRINT ?(R%+14)`);
+        expectArray(testMachine, [64, 0, 1, 64, 0, 1, 64, 0, 1, 64, 0, 1, 64, 64, 1]);
+    });
+    it("VIA.AC7 - getting to the bottom of ACR write at IRQ time #2", async function () {
+        const testMachine = await runViaProgram(`
+DIM MC% 256
+R% = ${resultAddress}
+P% = MC%
+[
+OPT 0
+SEI
+LDA #&7F
+STA &FE6E
+LDA #6
+STA &FE64
+
+LDA #&00
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&40
+STA &FE6B
+NOP : NOP : NOP : NOP
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%
+STX R%+1
+STY R%+2
+
+LDA #&00
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&40
+NOP : NOP : NOP : NOP
+STA &FE6B
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+3
+STX R%+4
+STY R%+5
+
+LDA #&00
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&40
+NOP : NOP : NOP
+STA &FE6B
+NOP
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+6
+STX R%+7
+STY R%+8
+
+LDA #&00
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&40
+LDX &00
+NOP : NOP : NOP
+STA &FE6B
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+9
+STX R%+10
+STY R%+11
+
+LDA #&00
+STA &FE6B
+LDA #&00
+STA &FE65
+
+LDA #&C0
+NOP : NOP : NOP : NOP
+STA &FE6B
+LDA &FE6D
+LDY &FE64
+NOP : NOP
+LDX &FE6D
+LDY &FE64
+STA R%+12
+STX R%+13
+STY R%+14
+CLI
+RTS
+]
+CALL MC%
+PRINT "VIA TEST: GETTING TO THE BOTTOM OF ACR WRITE AT IRQ TIME #2"
+PRINT "REAL BBC: 64, 64, 1, 64, 0, 1, 64, 64, 1, 64, 0, 1, 64, 0, 1"
+PRINT "00 -> 40 IMMEDIATE"
+REM REAL BBC! 64, 64, 1
+PRINT ?(R%)
+PRINT ?(R%+1)
+PRINT ?(R%+2)
+PRINT "00 -> 40 AT -1"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%+3)
+PRINT ?(R%+4)
+PRINT ?(R%+5)
+PRINT "00 -> 40 AT 0"
+REM REAL BBC! 64, 64, 1
+PRINT ?(R%+6)
+PRINT ?(R%+7)
+PRINT ?(R%+8)
+PRINT "00 -> 40 AT -1, 2 CYCLE STORE"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%+9)
+PRINT ?(R%+10)
+PRINT ?(R%+11)
+PRINT "00 -> C0 AT -1"
+REM REAL BBC! 64, 0, 1
+PRINT ?(R%+12)
+PRINT ?(R%+13)
+PRINT ?(R%+14)`);
+        expectArray(testMachine, [64, 64, 1, 64, 0, 1, 64, 64, 1, 64, 0, 1, 64, 0, 1]);
     });
 });
