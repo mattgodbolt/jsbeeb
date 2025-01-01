09dbaa45f315a5c7efbb680eb5fc8676f2715de9
Matt Godbolt <matt@godbolt.org>
2025-01-01 23:03:53 +0000

    Don't inline anything (well, really only audio workers). Fixes #468

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { configDefaults } from "vitest/config";
 
-/** @type {import('vite').UserConfig} */
+/** @type {import("vite").UserConfig} */
 export default {
     build: {
         sourcemap: true,
+        // Prevent inlining; we don't want any worklets/audio workers to be inlined as that doesn't work.
+        assetsInlineLimit: 0,
     },
     test: {
         include: [...configDefaults.include, "tests/unit/**/*.js", "tests/integration/**/*.js"],
