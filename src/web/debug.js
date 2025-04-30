addb3f5e81550bd6b8dea2552bc071f20c1cc06f
Matt Godbolt <matt@godbolt.org>
2025-04-30 10:55:23 -0500

    Fix substring issues (#482)
    In 59971a80fb22fa20ed1b9f230c4b0e46e641d49e i switching substr to substring
    but didn't realise the API changed. This fixes up the issues associated
    with that and also improves the affectd code for the debugger.
    
    Fixes #479

--- src/web/debug.js
+++ src/web/debug.js
@@ -1,6 +1,5 @@
 "use strict";
 import $ from "jquery";
-import _ from "underscore";
 import { hexbyte, hexword, noop, parseAddr } from "../utils.js";
 
 const numToShow = 16;
@@ -82,7 +81,7 @@ class MemoryView {
 export class Debugger {
     constructor(video) {
         this.video = video;
-        this.patchInstructions = {};
+        this.patchInstructions = new Map();
         this._enabled = false;
         this.disass = $("#disassembly");
         this._memoryView = new MemoryView($("#memory"), (address) => (this.cpu ? this.cpu.peekmem(address) : 0));
@@ -260,41 +259,36 @@ export class Debugger {
         }
     }
 
-    execPatch(inst) {
-        const insts = inst.split(",");
-        if (insts.length !== 1) {
-            _.each(insts, this.execPatch);
-            return;
-        }
-        if (!inst) return;
-        const ops = inst.split(":");
-        let addr = parseInt(ops[0], 16);
-        const setTo = ops[1];
-        for (let i = 0; i < setTo.length; i += 2) {
-            const b = parseInt(setTo.substring(i, 2), 16);
-            this.cpu.writemem(addr, b);
-            addr++;
+    execPatch(instString) {
+        for (const inst of instString.split(",")) {
+            if (!inst) continue;
+            const ops = inst.split(":");
+            let addr = parseInt(ops[0], 16);
+            const setTo = ops[1];
+            for (let i = 0; i < setTo.length; i += 2) {
+                const b = parseInt(setTo.substring(i, i + 2), 16);
+                this.cpu.writemem(addr, b);
+                addr++;
+            }
         }
     }
 
     setPatch(patch) {
-        _.each(patch.split(";"), (inst) => {
+        for (const inst of patch.split(";")) {
             if (inst[0] === "@") {
-                const at = parseInt(inst.substring(1, 4), 16);
-                inst = inst.substring(5);
-                if (!this.patchInstructions[at]) this.patchInstructions[at] = [];
-                this.patchInstructions[at].push(inst);
+                const at = parseInt(inst.substring(1, 5), 16);
+                if (!this.patchInstructions.has(at)) this.patchInstructions.set(at, []);
+                this.patchInstructions.get(at).push(inst.substring(5));
             } else {
                 this.execPatch(inst);
             }
-        });
-        if (Object.keys(this.patchInstructions).length !== 0) {
+        }
+        if (this.patchInstructions.size > 0) {
             const hook = this.cpu.debugInstruction.add((pc) => {
-                const insts = this.patchInstructions[pc];
-                if (!insts) return false;
-                _.each(insts, this.execPatch);
-                delete this.patchInstructions[pc];
-                if (Object.keys(this.patchInstructions).length === 0) {
+                if (!this.patchInstructions.has(pc)) return false;
+                for (const inst of this.patchInstructions.get(pc)) this.execPatch(inst);
+                this.patchInstructions.delete(pc);
+                if (this.patchInstructions.size === 0) {
                     console.log("All patches done");
                     hook.remove();
                 }
